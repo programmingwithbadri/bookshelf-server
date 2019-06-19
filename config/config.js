0ba50205e00c0c3be8f63078902beadf46d932d9
Badri <badri.py@outlook.com>
2019-06-19 21:11:26 +0530

    Setup server.js and add config info for production and dev

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const config = {
+    production : {
+        SECRET: process.env.SECRET,
+        DATABASE: process.env.MONGODB_URI
+    },
+    default : {
+        SECRET: 'SUPERSECRETPWD12345',
+        DATABASE: 'mongodb://localhost:27017/bookShelf'
+    }
+}
+
+exports.get = function(env) {
+    return config[env] || config.default
+}
